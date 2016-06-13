c7803bc6a2c40e39255ff6a428efa5fbe64721cd
shaohuimao <shaohuimao@sohu-inc.com>
2016-06-14 06:25:43 +0800

    add

---
+++ src/main/java/Start.java
@@ -0,0 +1,32 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+/**
+ * Created by maoshaohui [maoshh88@gmail.com]
+ * on 16/6/13.下午8:04
+ */
+public class Start {
+
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup);
+
+        bootstrap.channel(NioServerSocketChannel.class);
+
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                //todo: add handler
+            }
+        });
+        bootstrap.bind(8080).sync();
+    }
+}
